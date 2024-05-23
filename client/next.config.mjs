308418e3dc85e525812a8e9abaeb97fa47283c3f
Costea Mihai Costin <mihai.costin1010@gmail.com>
2024-05-23 21:58:42 +0200

    add azure

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
+    reactStrictMode: true,
+    output: "standalone",
+    distDir: "build",
 };
 
 export default nextConfig;
